3ea9c945757b1ecd0d2151b196e34b61ac86a5a2
Mariceldominguez <maricel.dominguez@davinci.edu.ar>
2026-02-10 12:27:39 -0300

    Update Catalog.tsx

--- src/pages/home/components/Catalog.tsx
+++ src/pages/home/components/Catalog.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getProductos, type StrapiProducto } from "@/services/api";
 import ProductCard from "./ProductCard";
-import Container from "../container";
+import Container from "../Container";
 
 
 // deploy fix
